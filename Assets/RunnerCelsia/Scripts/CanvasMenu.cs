e72df4ab3172ceba9854413858647dba296a21c2
QuatioDevelop <87876931+QuatioDevelop@users.noreply.github.com>
2022-06-23 10:22:22 -0500

    update rank

--- Assets/RunnerCelsia/Scripts/CanvasMenu.cs
+++ Assets/RunnerCelsia/Scripts/CanvasMenu.cs
@@ -17,12 +17,22 @@ public class CanvasMenu : MonoBehaviour
     public TextMeshProUGUI n4;
     public TextMeshProUGUI n5;
 
+    public TextMeshProUGUI n12;
+    public TextMeshProUGUI n22;
+    public TextMeshProUGUI n32;
+    public TextMeshProUGUI n42;
+    public TextMeshProUGUI n52;
+
     //public List<string> stringscores = new List<string>();
     //public List<float> scores = new List<float>();
     //public List<int> index = new List<int>();
 
+    public string[] stringnames;
+    public string[] sortstringnames;
+
     public string[] stringscores;
     public string[] sortstringscores;
+
     public float[] scores;
     public float[] sortscores;
 
@@ -103,6 +113,9 @@ public class CanvasMenu : MonoBehaviour
     {
         countRecords = PlayerPrefs.GetInt("count");
 
+        stringnames = new string[countRecords];
+        sortstringnames = new string[countRecords];
+
         stringscores = new string[countRecords];
         sortstringscores = new string[countRecords];
 
@@ -112,7 +125,8 @@ public class CanvasMenu : MonoBehaviour
         for (int i = 1; i <= countRecords; i++)
         {
             scores[i-1] = PlayerPrefs.GetFloat("Score" + i.ToString());
-            stringscores[i-1] = " " + PlayerPrefs.GetString("Nombre" + i.ToString()) + " - " + PlayerPrefs.GetString("Cedula" + i.ToString()) + " - " + PlayerPrefs.GetFloat("Score" + i.ToString()).ToString("00");
+            stringscores[i-1] = PlayerPrefs.GetFloat("Score" + i.ToString()).ToString("00");
+            stringnames[i-1] = " " + PlayerPrefs.GetString("Nombre" + i.ToString());
         }
 
         for (int i = 0; i < scores.Length; i++)
@@ -126,17 +140,27 @@ public class CanvasMenu : MonoBehaviour
                 if (scores[s] == maxValue)
                 {
                     scores[s] = 0;
+
                     sortstringscores[i] = stringscores[s];
                     stringscores[s] = "";
+
+                    sortstringnames[i] = stringnames[s];
+                    stringnames[s] = "";
                 }
             }
         }
 
-        n1.text += sortstringscores[0];
-        n2.text += sortstringscores[1];
-        n3.text += sortstringscores[2];
-        n4.text += sortstringscores[3];
-        n5.text += sortstringscores[4];
+        n1.text += sortstringnames[0];
+        n2.text += sortstringnames[1];
+        n3.text += sortstringnames[2];
+        n4.text += sortstringnames[3];
+        n5.text += sortstringnames[4];
+
+        n12.text += sortstringscores[0];
+        n22.text += sortstringscores[1];
+        n32.text += sortstringscores[2];
+        n42.text += sortstringscores[3];
+        n52.text += sortstringscores[4];
         
     }
 
