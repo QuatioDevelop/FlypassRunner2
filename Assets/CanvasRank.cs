974f99d5b694be6ad5d4953d7eb8a2b54a30a14e
QuatioDevelop <87876931+QuatioDevelop@users.noreply.github.com>
2022-06-23 12:38:42 -0500

    update

--- Assets/CanvasRank.cs
+++ Assets/CanvasRank.cs
@@ -77,7 +77,7 @@ public class CanvasRank : MonoBehaviour
         for (int i = 1; i <= countRecords; i++)
         {
             scores[i - 1] = PlayerPrefs.GetFloat("Score" + i.ToString());
-            stringscores[i - 1] = PlayerPrefs.GetFloat("Score" + i.ToString()).ToString("00");
+            stringscores[i - 1] = PlayerPrefs.GetFloat("Score" + i.ToString()).ToString();
             stringnames[i - 1] = " " + PlayerPrefs.GetString("Nombre" + i.ToString());
             stringdocs[i - 1] = " " + PlayerPrefs.GetString("Cedula" + i.ToString());
             stringemails[i - 1] = " " + PlayerPrefs.GetString("Correo" + i.ToString());
